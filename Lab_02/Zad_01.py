91274c2d661fb67be9f905ad850533724cb02ef9
Dejwid <s205915@sggw.edu.pl>
2023-08-21 09:06:54 +0200

    Statistical measures

---
+++ Lab_02/Zad_01.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import scipy.stats as stats
+import numpy as np
+
+data = pd.read_excel("dane.xlsx")
+x = data["Dochód"]
+y = data["Spożycie mięsa"]
+
+b1, b2, r, p, std_error = stats.linregress(x, y)
+#b1, b2 = np.polyfit(x, y, 1)
+
+y_pred = b1 * x + b2
+y_mean = np.mean(y)
+r_squared = np.sum((y_pred - y_mean) ** 2) / np.sum((y - y_mean)**2) 
+
+n = data.shape[0]
+k = 1
+se = np.sqrt(np.sum((y - y_pred) ** 2) / (n - k - 1))
+
+mae = np.mean(np.abs(y_pred-y))
+wzl = se / y_mean
+
+f = (n - k - 1) / k * r_squared/(1 - r_squared)
+f_s = stats.f.ppf(1 - 0.05, k, n - 2)
+
+if f > f_s:
+    print("Odrzucamy hipotezę zerową: parametr jest statystycznie isototny")
+else:
+    print("Brak podstaw do odrzucenia: parametr jest statystycznie nieistotny")
+
+
+
+print(f"Model: {round(b1,2)}x + {round(b2,2)}")
+#print(f"Model: {round(b1,2)}x + {round(b2,2)}")
+print(f"Interpretacja parametru b1: Jeśli dochód wzrośnie o jednostkę to sporzycie mięsa wzrośnie średnio o {b1} kg na osobę")
+print("R-value: ", r)
+print("p-value: ", p)
+print("Standard error of the parametr: ", std_error)
+print(f"R-squared: {r_squared}, zatem {r_squared} całkowitej zmienności sporzycia mięsa (y) zostało wyjaśnione modelem. " )
+print(f"Standard error: {se}, zatem wartości empiryczne spożycia mięsa (y) odchylają się przeciennie(średnio)" 
+      f" o {se} kilograma na osobę od wartości  teoretycznych wyznaczonych na podstawie modelu")
+print(f"Min absolute error: {mae}, czyli średnia z wartości bezwzględnych reszt wynosi {mae}")
+print(f"Coefficient of random variation: {wzl}. Odchylenie standardowe reszt stanowi {wzl * 100}% średniej wartości sporzycia mięsa."
+      "Dopasowanie modelu można uznać za dostatecznie dobre")
+print("Test F: ")
+if f > f_s:
+    print("Odrzucamy hipotezę zerową: parametr jest statystycznie isototny")
+else:
+    print("Brak podstaw do odrzucenia: parametr jest statystycznie nieistotny")
