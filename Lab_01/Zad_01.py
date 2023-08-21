cc9b46bceafc89fb3ec7f739ba30a74cd20c2677
Dejwid <s205915@sggw.edu.pl>
2023-08-21 08:56:18 +0200

    ttest and plot

---
+++ Lab_01/Zad_01.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+dane = pd.read_excel("dane.xlsx")
+dochod = dane["Dochód"]
+mieso = dane["Spożycie mięsa"]
+
+
+# test t
+t_statistic, p_value = stats.ttest_ind(dochod, mieso)
+
+print("Wartość statystyki t:", t_statistic)
+print("Wartość p:", p_value)
+
+alpha = 0.05
+if p_value < alpha:
+    print("Odrzucamy hipotezę zerową - istnieje statystycznie istotna różnica między grupami.")
+else:
+    print("Nie ma podstaw do odrzucenia hipotezy zerowej - brak statystycznie istotnej różnicy między grupami.")
+
+
+# wykres
+plt.figure(figsize = (10,8))
+plt.scatter(dochod, mieso, color = "blue")
+plt.xlabel("Dochód [zł]")
+plt.ylabel("Spożycie mięsa [kg]")
+plt.title("ZALEŻNOŚĆ SPOŻYCIA MIĘSA OD DOCHODÓW")
+plt.show()
