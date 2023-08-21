031dad94cd77a6a9917d1d5b71b7e8af4e9e64ba
Dejwid <s205915@sggw.edu.pl>
2023-08-21 19:45:54 +0200

    quasi stałe

---
+++ Lab_05/Quasi_stałe_02.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = pd.read_excel("data.xlsx", sheet_name = "dane_02")
+v_critical = 0.15
+variables = []
+n = data.shape[1]
+
+for i in range(1, n + 1):
+    mean = data[f'X{i}'].mean()
+    data[f'(X{i}-Xśr)^2'] = (data[f'X{i}'] - mean) ** 2
+    S = (sum(data[f'(X{i}-Xśr)^2'])/data[f'X{i}'].shape[0]) ** (1/2)
+    V = S/mean
+    if abs(V) > v_critical:
+        variables.append(f'X{i}')
+
+print("Wybrane zmienne: ", variables)
