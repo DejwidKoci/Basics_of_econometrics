0239b462d62a5550c4ebd2c6bf1dc063350af96b
Dejwid <s205915@sggw.edu.pl>
2023-08-24 21:21:53 +0200

    Goldfeld_Quandt

---
+++ Lab_09/Goldfeld_Quandt.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.stats.api import het_goldfeldquandt
+
+data = pd.read_excel('data.xlsx', sheet_name = 'dane_01')
+y = data['Wydatki w zł']
+X = data[['Dochody w zł']]
+X = sm.add_constant(X)
+model = sm.OLS(y, X).fit()
+y_predicted = model.predict(X)
+
+residuals = y - y_predicted
+
+test_result = het_goldfeldquandt(residuals, X)
+
+print("Wynik testu Goldfelda-Quandta:")
+print("Statystyka testowa:", test_result[0])
+print("P-wartość:", test_result[1])
+
+alpha = 0.05  
+
+if test_result[1] > alpha:
+    print("Nie ma podstaw do odrzucenia hipotezy zerowej: Reszty są homoskedastyczne.")
+else:
+    print("Odrzucamy hipotezę zerową: Reszty nie są homoskedastyczne.")
